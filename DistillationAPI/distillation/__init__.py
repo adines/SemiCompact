aeef3c4b3f9e288ff8e67c57a8ab50cde42ff923
Adrián Inés <adines@unirioja.es>
2021-11-03 16:38:15 +0100

    Changed pureDistillation by plainDistillation

--- DistillationAPI/distillation/__init__.py
+++ DistillationAPI/distillation/__init__.py
@@ -2,7 +2,7 @@ from distillation.semiSupervised import *
 
 __all__=['pureDistillation','dataDistillation','modelDistillation','modelDataDistillation']
 
-pureDistillation.__module__='distillation'
+plainDistillation.__module__='distillation'
 dataDistillation.__module__='distillation'
 modelDistillation.__module__='distillation'
 modelDataDistillation.__module__='distillation'
